551da5cf1875f87f8b22e97f59f72c1f141e3528
Grace Hephzibah M <63595093+Grace-Hephzibah@users.noreply.github.com>
2021-08-06 12:34:12 +0530

    Update Anagram.c

--- HashTables/Anagram.c
+++ HashTables/Anagram.c
@@ -1,6 +1,45 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+int anagram(char *s1, char *s2)
+{
+    int hash1[26] = {0}, hash2[26] = {0}, l1 = strlen(s1), l2 = strlen(s2) ,check = 1, x;
+    
+    if(l1!=l2) return 0;
+    
+    for(int i=0; i<l1; i++)
+    {   
+        x = (s1[i] > 96) ? 97 : 65;
+        hash1[s1[i] - x]++;
+        x = (s2[i] > 96) ? 97 : 65;
+        hash2[s2[i] - x]++;
+    }
+    
+    for(int i=0; i<26; i++)
+    {
+        if(hash1[i] != hash2[i])
+        {check = 0; break;}
+    }
+    
+    return check;
+}
+
+int main()
+{
+    char *s1 = malloc(100 * sizeof(char)), *s2 = malloc(100 * sizeof(char));
+    printf("Enter the 2 strings : ");
+    scanf("%s %s", s1,s2);
+    
+    if(anagram(s1, s2))  
+         printf("Hola ! They form an Anagram");
+    else printf("Alas! They do not form an Anagram");
+    return 0;
+}
+
+/*
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 int anagram(char *s1, char *s2)
 {
@@ -41,3 +80,4 @@ int main()
     else printf("Alas! They do not form an Anagram");
     return 0;
 }
+*/
